22a07cc2adcb6147038fe4be2d5e4bc42ed22e11
Souvik Kundu <kivtech88@gmail.com>
2019-08-04 00:20:49 +0530

    commit

---
+++ findNemo.js
@@ -0,0 +1,12 @@
+const everyone = ['gary', 'bruce', 'helen', 'hamilton', 'joyee', 'tom', 'tim', 'nemo', 'patty', 'jessica', 'jen'];
+function findNemo(array) {
+    for(i=0; i < array.length; i++) {
+        console.log('running');
+        if(array[i] === 'nemo') {
+            console.log('we found nemo');
+            break;
+        }
+    }
+}
+
+findNemo(everyone);
