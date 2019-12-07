6d1fc89f92c721220e7c4cc79b337c96ab707c76
Souvik Kundu <kivtech88@gmail.com>
2019-12-07 18:22:39 +0530

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -1,18 +1,52 @@
 // create a function that reverses a string
-// reverse this string --> 'Hi, my name is Souvik'
+// output: reverse this string --> 'Hi, my name is Souvik'
+
+// function reverse(str) {
+//     // check input
+//     if (!str || str.length < 2 || typeof str !== 'string') {
+//         return 'That is not correct. Invalid entry';
+//     }
+//     const backwards = [];
+//     const totalItems = str.length - 1;
+//     for (let i = totalItems; i >= 0; i--) {
+//         backwards.push(str[i]);
+//     }
+//     console.log(backwards);
+//     return backwards.join('');
+// }
+// reverse('Hi! My name is Souvik.');
 
 function reverse(str) {
-    // check input
-    if (!str || str.length < 2 || typeof str !== 'string') {
-        return 'That is not correct. Invalid entry';
+    // check correct input
+    if (!str || str.length < 1 || typeof str !== 'string') {
+        return 'not a correct input, try again';
     }
     const backwards = [];
-    const totalItems = str.length - 1;
-    for (let i = totalItems; i >= 0; i--) {
+    const totalItems = str.length - 1
+    for (let i = totalItems; i >= 0; i--) { // loop through the array in reverse
         backwards.push(str[i]);
     }
-    //console.log(backwards);
     return backwards.join('');
 }
 
-reverse('Hi! My name is Souvik.');
+reverse('Hello, how are you?');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
