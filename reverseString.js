d390f1fa6984e97164244b85d1f10c9b29f84dbb
Souvik Kundu <kivtech88@gmail.com>
2019-12-07 16:54:30 +0530

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -3,5 +3,11 @@
 
 function reverse(str) {
     // check input
-    if (!str || str.length || )
+    if (!str || str.length < 1 || typeof str !== 'string') {
+        return 'That is not correct. Invalid entry';
+    }
+    
+    const backwards = [];
+    const totalItems = str.length - 1;
+    for (let i = totalItems)
 }
