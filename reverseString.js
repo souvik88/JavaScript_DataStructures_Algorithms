6b74afd9ece089470462733858d1e963e9a13cfa
Souvik Kundu <kivtech88@gmail.com>
2019-12-07 20:17:11 +0530

    Update reverseString.js

--- reverseString.js
+++ reverseString.js
@@ -16,29 +16,30 @@
 // }
 // reverse('Hi! My name is Souvik.');
 
-function reverse(str) {
+function reverse1(str) {
     // check correct input
-    if (!str || str.length < 1 || typeof str !== 'string') {
-        return 'not a correct input, try again';
+    if (!str || str.length < 5 || typeof str !== 'string') {
+        console.log('not a correct input, try again');
+    } else {
+        const backwards = [];
+        const totalItems = str.length - 1
+        for (let i = totalItems; i >= 0; i--) { // loop through the array in reverse
+            backwards.push(str[i]);
+        }
+        console.log(backwards.join(''));
     }
-    const backwards = [];
-    const totalItems = str.length - 1
-    for (let i = totalItems; i >= 0; i--) { // loop through the array in reverse
-        backwards.push(str[i]);
-    }
-    console.log(backwards.join(''));
 }
-reverse('Hello, how are you?');
+reverse1('q');
 
+// reverse2 - a shorter version
 function reverse2(str) {
     if (!str || str.length < 5 || typeof str !== 'string') {
         console.log('not a correct input, try again');
     } else {
         console.log(str.split('').reverse().join(''));
     }
-
 }
-reverse2(3);
+reverse2('This is reverse 2');
 
 
 
